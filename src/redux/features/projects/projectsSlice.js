745dc8aa4ae3dd83de89130632853d3ee9bff9f8
Cristian Bermudez <ccbabcn@gmail.com>
2022-05-10 01:57:19 +0200

    implmented initial app test and improved test on Todo an toDoList

--- src/redux/features/projects/projectsSlice.js
+++ src/redux/features/projects/projectsSlice.js
@@ -5,9 +5,14 @@ const projectSlice = createSlice({
   initialState: [],
   reducers: {
     loadToDos: (toDos, action) => [...action.payload],
+    deleteToDo: (toDos, action) =>
+      toDos.filter((toDo) => toDo.id !== action.payload),
   },
 });
 
-export const { loadToDos: loadToDosActionCreator } = projectSlice.actions;
+export const {
+  loadToDos: loadToDosActionCreator,
+  deleteToDo: deleteToDoActionCreator,
+} = projectSlice.actions;
 
 export default projectSlice.reducer;
