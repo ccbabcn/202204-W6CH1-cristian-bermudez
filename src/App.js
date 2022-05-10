2ba734a3b37f5c7d1f73558ba254596d4e684aec
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-10 19:45:02 +0200

    configured .env and loadToDosThunk

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { useDispatch } from "react-redux";
 import ToDoList from "./component/ToDoList/ToDoList";
 import { loadToDosActionCreator } from "./redux/features/projects/projectsSlice";
 import { initialtoDos } from "./data/data";
+import { loadToDosThunk } from "./redux/thunks/projectsThunks";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadToDosActionCreator(initialtoDos));
+    dispatch(loadToDosThunk(dispatch));
   }, [dispatch]);
 
   return (
